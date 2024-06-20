14117688efdf1afcf9d2e2cd32b4f286766eb719
Park Hyewon <db1648@naver.com>
2024-06-20 23:37:19 +0900

    method 기본개념

---
+++ src/array/Array1.java
@@ -0,0 +1,16 @@
+package array;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int student1 =90;
+        int student2 =80;
+        int student3 =70;
+        int student4 =75;
+
+        System.out.println("학생1 점수는 : " + student1);
+        System.out.println("학생2 점수는 : " + student1);
+        System.out.println("학생3 점수는 : " + student1);
+        System.out.println("학생4 점수는 : " + student1);
+
+    }
+}
